de886db8d74678fc1a7800a19116e09ccf01d3fd
proalamin-247 <pro.alamin247@gmail.com>
2022-03-26 01:23:47 +0600

    projects design and set up components folder also file

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Hy! wellcome</h2>
+            <h3>Try your luck</h3>
+            <p>look, this is Worlds Top value NFT collection. Now choose you nft and draw. than selected nft yours.........</p>
+        </div>
+    );
+};
+
+export default Header;
